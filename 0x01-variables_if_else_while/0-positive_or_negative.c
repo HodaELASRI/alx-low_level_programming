9faa0e5d244cd8af7376e3b8d49c84c56fd6c6f2
HODA EL ASRI <elasri.hoda@gmail.com>
2023-03-17 07:48:39 -0700

    ReCommit positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,20 +2,21 @@
 #include <time.h>
 #include <stdio.h>
 
+/*this function specify if a number is positive or negative or zero*/
+
 int main(void){
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	{
-		printf("%d is positive\n",n);
-}
-	else if (n<0)
-	{
-		printf("%d is negative\n",n);
-}
+	
+	if (n > 0){
+		printf("%d is positive\n" , n);
+	}
+	else if (n < 0){
+		printf("%d is negative\n" , n);
+	}
 	else {
-		printf("%d is Zero\n",n);
-}
+		printf("%d is zero\n" , n);
+	}
 	return (0);
 	}
