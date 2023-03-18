77ccc05d3e7d66694c9dc9f1dc01725bfc687edd
HODA EL ASRI <elasri.hoda@gmail.com>
2023-03-18 16:29:40 -0700

    commit 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function is the entry point of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int i, j;
+	for (i = 0; i < 10; i++){
+		for (j = i; j < 10; j++){
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+			putchar(j + '0');
+		}
+	}
+	return (0);
+}
