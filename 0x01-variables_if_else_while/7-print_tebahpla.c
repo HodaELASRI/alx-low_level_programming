820b19718907649d82d532c79948b177d4a58f55
HODA EL ASRI <elasri.hoda@gmail.com>
2023-03-18 16:03:52 -0700

    commit 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function is the entry point of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{       char letter;
+
+        letter = 'z';
+        do {
+                putchar(letter);
+                letter--;
+        } while (letter >= 'a');
+        putchar('\n');
+        return (0);
+}
