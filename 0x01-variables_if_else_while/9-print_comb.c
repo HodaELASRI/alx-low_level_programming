12900650763e27c90fbed1cd32b177842105b655
HODA EL ASRI <elasri.hoda@gmail.com>
2023-03-18 16:35:24 -0700

    commit 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,11 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	int i, j;
+{	int i;
 	for (i = 0; i < 10; i++)
 	{	putchar(i + '0');
 		putchar(',');
 		putchar(' ');
-		putchar('\n');
 	}
 	return (0);
 }
